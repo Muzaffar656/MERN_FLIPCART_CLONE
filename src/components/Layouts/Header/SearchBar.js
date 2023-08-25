ee9ddde68235ee3a8a743956fc676b84b75b3c3e
Muzaffar <hussainmuzaffar656@gmail.com>
2023-08-25 04:38:20 -0700

    rr

--- src/components/Layouts/Header/SearchBar.js
+++ src/components/Layouts/Header/SearchBar.js
@@ -3,8 +3,8 @@ import React from 'react'
 const SearchBar = () => {
   return (
 
-    <div className=' bg-white  sm:w-9/12 w-80 px-1 sm:px-4  overflow-hidden rounded-sm '>
-      <input className=' text-sm text-gray-500 py-2  outline-none w-full  border-none rounded-sm '  type="text" placeholder='Search for products for more' />
+    <div className=' bg-white font-sans   sm:w-72  px-1 sm:px-4  overflow-hidden rounded-sm '>
+      <input className=' text-sm text-gray-700 py-2  w-full outline-none   border-none rounded-sm '  type="text" placeholder='Search for products, brands and more' />
     </div>
   )
 }
