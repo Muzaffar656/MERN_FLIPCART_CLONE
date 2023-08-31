1be8cf2b7c2ae35f9b91f53ab78ae3f70167718a
Muzaffar <hussainmuzaffar656@gmail.com>
2023-08-31 00:18:08 -0700

    user

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -1,13 +1,22 @@
 const User  = require('../Models/UserModel')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const JWT_KEY = process.env.JWT_KEY 
 const Register = async(req,res)=>{
 
     const {name,email,password} = req.body;
+    if(!name || !email || !password){
+        res.status(400).json({Error:"All fields are mandatory!"})
+    }
+    const UserAlready = await User.findOne({email})
+    if(UserAlready){
+        res.status(400).json({Error:"User are Already Registerd!"})
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
     const user = await User.create({
         name,
         email,
-        password
+       password: hashedPassword
     })
     const JWT = jwt.sign({user:{
         name:user.name,
@@ -19,8 +28,27 @@ const Register = async(req,res)=>{
     res.json(JWT)
    
 }
-const Login = (req,res)=>{
-    res.json({Login:"True"})
+const Login = async(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        res.status(400).json({Error:"All fields are mandatory!"})
+    }
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password,user.password))){
+        res.status(200)
+        const JWT = jwt.sign({
+            user:{
+                name:user.name,
+                email:user.email,
+                id:user._id
+            }
+        },
+        JWT_KEY
+        )
+        res.status(200).json({JWT})
+    }else{
+        res.status(400).json({Error:"Email and Password is not valid"})
+    }
 }
 
 
