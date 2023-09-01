6d093eb4c9aa69432d1e94e3c5996dc295d5b4d4
Muzaffar <hussainmuzaffar656@gmail.com>
2023-09-01 05:03:43 -0700

    file upload complete

--- Backend/Controller/ProductController.js
+++ Backend/Controller/ProductController.js
@@ -1,12 +1,24 @@
 const cloudinary = require('cloudinary').v2
-
+const Product = require('../Models/ProductModel')
 
 const createProduct = async (req,res)=>{
-   const file = req.files.photo
-   const myimage = await cloudinary.uploader.upload(file.tempFilePath,(err,result)=>{
-    console.log(result)
-   })
- 
+    const file = req.files.images
+ const mycloud =   await cloudinary.uploader.upload(file.tempFilePath)
+    const {name,description,price,category} = req.body;
+    if(!name || !description || !price || !category ){
+        res.status(400).json({Error:"All Fields Are mendotry"})
+    }
+    const product = await Product.create({
+        name,
+        description,
+        price,
+        category,
+        images:{
+            public_id:mycloud.public_id,
+            url : mycloud.url
+        }
+    })
+    res.status(200).json({message:"Product Created Succesfully"})
 }
 
 module.exports = createProduct
